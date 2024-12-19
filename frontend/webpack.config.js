07e2eb105c0568fe74278dfc46ad6f21b06d0dc8
Ubuntu <ubuntu@ip-172-31-7-181.us-west-1.compute.internal>
2024-12-19 22:43:47 +0000

    backend+frontend templates

---
+++ frontend/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+    devServer: {
+    host: '0.0.0.0', // Bind to all interfaces
+    port: 8081, // Ensure the correct port
+    static: './dist', // Serve from the correct directory
+    open: false, // Optional, auto-opens browser
+  },
+
+};
