cd6500518733872a2cdf54e2422d3c4b2bf69185
Ubuntu <ubuntu@ip-172-31-7-181.us-west-1.compute.internal>
2024-12-20 00:10:18 +0000

    working frontend+backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+)*6w6r(xtrydhu*cl@w$3^@qz^ptwjs8bczdgj+1@=_=$88d#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.57.247.141', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -73,12 +73,19 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vijaydb',
+        'USER': 'admin',
+        'PASSWORD': 'vijaypasscode',
+        'HOST': 'vijay-db.cf0iauigcrc9.us-west-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
